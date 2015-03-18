917eab4571c799f7a06db4ea3a6047f2b3b384f4
czengg <czengg@CMU-896368.WV.CC.CMU.EDU>
2015-03-17 21:22:38 -0400

    routing

--- www/app/app.js
+++ www/app/app.js
@@ -1,7 +1,39 @@
 
-angular.module('ppcprototypeApp', [
+var app = angular.module('ppcprototypeApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
   'ngRoute',
-  'btford.socket-io']);
+  'btford.socket-io',
+  'ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise("/welcome");
+
+  $stateProvider
+    .state('welcome', {
+      url: "/welcome",
+      templateUrl: "templates/welcome.html"
+    })
+    .state('location', {
+      url: "/location",
+      templateUrl: "templates/location.html"
+    })
+    .state('overview', {
+      url: "/overview",
+      templateUrl: "templates/overview.html"
+    })
+    .state('content', {
+      url: "/content",
+      templateUrl: "templates/content.html"
+    })
+    .state('congratulations', {
+      url: "/congratulations",
+      templateUrl: "templates/congratulations.html"
+    })
+    .state('tester', {
+      url: "/tester",
+      templateUrl: "templates/tester.html"
+    });
+});
