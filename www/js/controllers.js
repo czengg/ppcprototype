ce794a604154cb086f17a9f802a61685205f2f65
czengg <czengg@CMU-896368.WV.CC.CMU.EDU>
2015-03-18 01:56:04 -0400

    welcome, location, overview done

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,3 +5,20 @@ angular.module('ppcprototypeApp')
   console.log('TestCtrl');
   console.log(ContentFactory);
 }])
+
+.controller('LocationCtrl', [ '$scope', '$mdDialog', '$location', function ($scope, $mdDialog, $location) {
+  $scope.showConfirm = function(ev) {
+    var confirm = $mdDialog.confirm()
+      .content('This App Would Like to Use Your Current Location')
+      .ok('OK')
+      .cancel("Don't Allow")
+      .targetEvent(ev);
+    $mdDialog.show(confirm).then(function() {
+      $location.url('/overview')
+    });
+  };
+}])
+
+.controller('OverviewCtrl', [ '$scope', 'ContentFactory', function ($scope, ContentFactory) {
+  $scope.items = ContentFactory;
+}]);
